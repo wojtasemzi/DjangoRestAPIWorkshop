34eb315403773f8f1787f25d9924a34ee9521d24
Wojciech <wojciech.wantuch@gmail.com>
2022-02-19 14:25:00 +0100

    ass to utils add_screening

--- showtimes/tests/utils.py
+++ showtimes/tests/utils.py
@@ -1,9 +1,23 @@
 from random import sample
+from faker import Faker
 
 from movies import models as models_movies
+from showtimes import models as models_showtimes
+
+faker = Faker('pl_PL')
 
 
 def random_movies(numer_of_movies=3):
     """ Return 3 (by default) random Movie objects. """
     movies = list(models_movies.Movie.objects.all())
     return sample(movies, numer_of_movies)
+
+
+def add_screening(cinema, number_of_movies=3):
+    """ Adds 3 (by default) screenings for provided cinema. """
+    movies = random_movies(number_of_movies)
+    for movie in movies:
+        models_showtimes.Screening.objects.create(
+                cinema=cinema,
+                movie=movie,
+                date=faker.date_time_between())
